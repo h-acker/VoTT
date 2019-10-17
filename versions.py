2a671e5144ef376c8ec8a165383cca0ade2517c6
ebreton <emmanuel.breton@cortexia.ch>
2019-10-17 16:26:07 +0200

    Remove reference to backend

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.0-4-g8c63c1a'
+_release = '0.3.0-6-gd987b4b'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '0.3.1'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '8c63c1a17a7fbb654f9d6157624e8aecce3d8415'
+_build = 'd987b4b0a1db64a28f949327d7e94724344f56a1'
