186bb38b4214f762c38fb3dc1907757f9e713c12
ebreton <email>
2019-10-17 07:49:19 +0200

    Prepare version 0.3.1

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.0'
+_release = '0.3.0-2-gf49d0b8'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.3.1-rc'
+_version = '0.3.1'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '2bd58d321eb69ee413b71b7cac7f73d5d12d30f6'
+_build = 'f49d0b8e213bbbbf392726dd2e7080ce9e8cb8ae'
