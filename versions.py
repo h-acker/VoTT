b8240a4ab33513cbb4f4c724e0517efe6e2a13c2
Schnaebele <marc.schnaebele@cortexia.ch>
2019-10-18 11:22:12 +0200

    Prepared release 0.3.2

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.3.0-16-ga352b16'
+_release = '0.3.1'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.3.1'
+_version = '0.3.2'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'a352b1692fd4c2e825260c9e0d06093d20e01d52'
+_build = 'd8948270c5f63b4529669528ad8a4bbb8f1cfb73'
