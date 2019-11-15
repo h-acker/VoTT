565b1f675b92f0c258a2169aa1818f248e427a7d
mateuszpmroz <mateuszpmroz@gmail.com>
2019-11-15 07:09:52 +0100

    126 add cortexia version

--- src/react/components/shell/statusBar.tsx
+++ src/react/components/shell/statusBar.tsx
@@ -11,7 +11,7 @@ export class StatusBar extends React.Component {
                     <ul>
                         <li>
                             <i className="fas fa-code-branch"></i>
-                            <span>{appInfo.version}</span>
+                            <span>{appInfo.version}-{process.env.REACT_APP_CORTEXIA_VERSION}</span>
                         </li>
                     </ul>
                 </div>
