68f5c0146dad9c29ef064b286c50f0cf634c6612
ebreton <email>
2019-11-23 15:45:47 +0100

    Update version to 0.6.1-rc

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.5.0-10-ga96f9c0'
+_release = '0.5.0-11-geb2a6a2'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.5.1-rc'
+_version = '0.6.1-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'a96f9c0f54d241a911d3a1dea7a0987ff4981fe2'
+_build = 'eb2a6a28d5c6918c66005093bd6f2bc8011fb015'
