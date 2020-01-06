ff7902d054ac89ca7e5cafadd406bf2c48fcb222
ebreton <email>
2020-01-06 20:07:06 +0100

    Change version format

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '0.7.0-2-g1becca3'
+_release = '0.7.0-3-g728f5c2'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '0.7.1-rc'
+_version = '2020.01.0'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '1becca338923c3d014f8d98fe14c842e45674672'
+_build = '728f5c2e88a858f4b397516553482edb3996a1ca'
