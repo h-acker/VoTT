e14d647ce14de3f9a35c9d85146ad2b391be00b5
Schnaebele <marc.schnaebele@cortexia.ch>
2020-03-03 14:34:39 +0100

    fix update release using wrong versions file (.pyc, .py)

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '2020.2.2-18-g3f8f5cd'
+_release = '2020.2.4-rc-1-gafe682e'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '2020.2.4-rc'
+_version = '2020.2.5-rc'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '3f8f5cd69176c6f90577a578fce7b7cfe5d75a51'
+_build = 'afe682ee166f07fff3f71d784267a364295fe096'
