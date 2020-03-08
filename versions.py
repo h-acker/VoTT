f76d53c2980457236a08fa79add5b926fce6035b
Schnaebele <marc.schnaebele@cortexia.ch>
2020-03-08 10:16:22 +0100

    Prepared release 2020.3.1

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '2020.2.4-rc-1-gafe682e'
+_release = '2020.2.4-rc-2-ge14d647'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '2020.2.5-rc'
+_version = '2020.3.1'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'afe682ee166f07fff3f71d784267a364295fe096'
+_build = 'e14d647ce14de3f9a35c9d85146ad2b391be00b5'
