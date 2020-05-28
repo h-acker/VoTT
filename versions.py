4207a261e45582d5702bbf50e6396d6b63aa287f
johannamelly <email>
2020-05-28 09:05:35 +0200

    Prepared release 2020.5.3

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '2020.5.0-3-gb2f0cf8'
+_release = '2020.5.1-6-g3f7c0fd'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '2020.5.1'
+_version = '2020.5.3'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'b2f0cf80fbba7e45d16cab677d6479e44da7086e'
+_build = '3f7c0fdcbcc085090e6e9acaa5609dd7f4916a4a'
