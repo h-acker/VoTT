590e9cf13392836fbe79a0bea240b63d0a158a37
johannamelly <email>
2021-09-23 07:36:08 +0200

    Prepared release 2021.9.1

--- versions.py
+++ versions.py
@@ -3,14 +3,14 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '2021.3.2-6-gd77e074'
+_release = '2021.5.1-15-gfabdddc'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
 # => read-write, >_release
-_version = '2021.5.1'
+_version = '2021.9.1'
 
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = 'd77e0741d4fabb44e9c39f6b69f028f96cf0b662'
+_build = 'fabdddca679d654de3253a40dbf848f256d96927'
