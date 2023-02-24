ec1c0e4c9fb8c376db26496c396034910cc7b3d5
johannamelly <email>
2023-02-24 11:26:14 +0100

    deleted useless part in config, roll back environement

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,17 +1,4 @@
 const path = require("path");
-
-const fallback = config.resolve.fallback || {};
-Object.assign(fallback, {
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-    assert: require.resolve("assert"),
-    http: require.resolve("stream-http"),
-    https: require.resolve("https-browserify"),
-    os: require.resolve("os-browserify"),
-    url: require.resolve("url")
-});
-
-config.resolve.fallback = fallback;
 module.exports = {
     node: {
         __dirname: false
