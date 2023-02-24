81cd4ce8c40c22fd3cf5175923b568503120d3c0
johannamelly <email>
2023-02-24 11:22:39 +0100

    lint

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,20 +2,19 @@ const path = require("path");
 
 const fallback = config.resolve.fallback || {};
 Object.assign(fallback, {
-  crypto: require.resolve("crypto-browserify"),
-  stream: require.resolve("stream-browserify"),
-  assert: require.resolve("assert"),
-  http: require.resolve("stream-http"),
-  https: require.resolve("https-browserify"),
-  os: require.resolve("os-browserify"),
-  url: require.resolve("url"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify"),
+    url: require.resolve("url")
 });
 
 config.resolve.fallback = fallback;
 module.exports = {
-
     node: {
-        __dirname: false,
+        __dirname: false
     },
     target: "electron-main",
     entry: "./src/electron/main.ts",
@@ -23,14 +22,16 @@ module.exports = {
         rules: [
             {
                 test: /\.ts?$/,
-                use: [{
-                    loader: "ts-loader",
-                    options: {
-                        compilerOptions: {
-                            noEmit: false
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            compilerOptions: {
+                                noEmit: false
+                            }
                         }
                     }
-                }],
+                ],
                 exclude: /node_modules/
             }
         ]
@@ -38,10 +39,10 @@ module.exports = {
     resolve: {
         extensions: [".ts", ".js", ".json"],
         fallback: {
-            "fs": require.resolve("fs"),
-            "os": require.resolve("os-browserify/browser"),
-            "path": require.resolve("path-browserify"),
-            "buffer": require.resolve("buffer/")
+            fs: require.resolve("fs"),
+            os: require.resolve("os-browserify/browser"),
+            path: require.resolve("path-browserify"),
+            buffer: require.resolve("buffer/")
         }
     },
     output: {
