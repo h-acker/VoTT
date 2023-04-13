3de36c840187421731d3e6de06c761611a2d3e02
johannamelly <email>
2023-04-13 21:21:59 +0200

    Prepared release 2023.4.1

--- versions.py
+++ versions.py
@@ -3,7 +3,7 @@
 
 # the release comes from git and should not be modified
 # => read-only
-_release = '2023.2.2-7-g80c6eb3'
+_release = '2023.3.1-3-g8876127'
 
 # you can set the next version number manually
 # if you do not, the system will make sure that version > release
@@ -13,4 +13,4 @@ _version = '2023.4.1'
 # the build number will generate conflicts on each PR merge
 # just keep yours every time
 # => read-only
-_build = '80c6eb33c003a807b5bcf1e24b28b650a2422e7f'
+_build = '887612763154155738848bd46df0960c3baef855'
